52239b68611a078d40d8b7e8f65e9b6e604791e2
Dmytro Ivashchuk <dimaiv@me.com>
2020-01-03 21:33:01 +0100

    add profile card to the page

--- src/components/mentorCard.js
+++ src/components/mentorCard.js
@@ -1,7 +1,70 @@
 import React from "react"
+import { useStaticQuery, graphql } from "gatsby"
+import Img from "gatsby-image"
+import styled from "styled-components"
+
+import { Typography, Tag } from "antd"
+
+const { Title } = Typography
+
+const StyledInfo = styled.div`
+  margin-bottom: 30px;
+`
+
+const StyledImage = styled(Img)`
+  height: 160px;
+  width: 160px;
+  margin: auto;
+  border-radius: 100px;
+`
+const StyledStack = styled.div`
+  display: flex;
+  justify-content: center;
+  margin-top: 10px;
+  flex-wrap: wrap;
+  span {
+    margin-bottom: 6px;
+  }
+`
 
 const MentorCard = () => {
-  return <div>Myself</div>
+  const data = useStaticQuery(graphql`
+    query ProfileImage {
+      file(relativePath: { eq: "profile.jpg" }) {
+        id
+        childImageSharp {
+          id
+          fluid {
+            ...GatsbyImageSharpFluid_tracedSVG
+          }
+        }
+      }
+    }
+  `)
+
+  return (
+    <StyledInfo>
+      {" "}
+      <StyledImage fluid={data.file.childImageSharp.fluid} />
+      <Title style={{ textAlign: "center" }} level={3}>
+        {" "}
+        Dimitri Ivashchuk
+      </Title>
+      <div style={{ textAlign: "center" }}>
+        Fullstack engineer/ blogger /egghead.io instructor
+      </div>
+      <StyledStack>
+        <Tag color="red">react</Tag>
+        <Tag color="magenta">graphql</Tag>
+        <Tag color="blue">docker</Tag>
+        <Tag color="purple">gatsby</Tag>
+        <Tag color="gold">serverless</Tag>
+        <Tag color="green">mongoDB</Tag>
+        <Tag color="orange">node</Tag>
+        <Tag color="gray">cypress</Tag>
+      </StyledStack>
+    </StyledInfo>
+  )
 }
 
 export default MentorCard
